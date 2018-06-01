da01d7ac76a7a06caa00c1fec13cd71f6e1b6599
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2018-06-01 12:54:12 +0530

    + Added Registry using genserver and test cases + Added bucket using agent and test cases

---
+++ lib/kv/registry.ex
@@ -0,0 +1,36 @@
+defmodule KV.Registry do
+  use GenServer
+
+  ## Client API
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  ## Server API
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:lookup, name}, _from, names) do
+    {:reply, Map.fetch(names, name), names}
+  end
+
+  def handle_cast({:create, name}, names) do
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.start_link([])
+      {:noreply, KV.Bucket.put(names, name, bucket)}
+    end
+  end
+end
---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,19 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+
+    KV.Bucket.put(bucket, "water", 2)
+    KV.Bucket.delete(bucket, "water")
+    assert KV.Bucket.get(bucket, "water") == nil
+  end
+end
