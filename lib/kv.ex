1dd4512e6bda3a1aae5e58d061d0f26b69858701
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2018-06-04 16:06:22 +0530

    Start supervisor on start of application

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,4 +1,6 @@
 defmodule KV do
+  use Application
+
   @moduledoc """
   Documentation for KV.
   """
@@ -14,5 +16,9 @@ defmodule KV do
   """
   def hello do
     :world
+  end
+
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
   end
 end
